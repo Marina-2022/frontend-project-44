939f240af9844893c68f2b0393480bb0e5026720
Marina Garipova <naumarina@yandex.ru>
2023-09-09 16:47:11 +0800

    update index.js: add general logic for the games

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const startGame = () => {
+const startGame = (expressionsAndResults, task) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}`);
-  return userName;
+  console.log(task);
+  const raund = 3;
+  for (let i = 0; i < raund; i += 1) {
+    console.log(`Question: ${expressionsAndResults[0][i]}`);
+    const answer = (readlineSync.question('Your answer: '));
+    if (expressionsAndResults[1][i] === answer) {
+      console.log('Correct!');
+    } else if (expressionsAndResults[1][i] !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expressionsAndResults[1][i]}'`);
+      return console.log(`Let's try again, ${userName}!`);
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+const getEvenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
 };
 
 const getRandomValue = (min, max) => {
@@ -27,4 +46,18 @@ const calculator = (a, b, operator) => {
   }
 };
 
+const gcd = (number1, number2) => {
+  let a = number1;
+  let b = number2;
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a %= b;
+    } else if (b > a) {
+      b %= a;
+    }
+  }
+  return a + b;
+};
+
 export { startGame, getRandomValue, calculator };
+export { gcd, getEvenOrOdd };
