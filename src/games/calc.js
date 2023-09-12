29caae60e0662af9744f8700317c5f463e36b1fa
Marina Garipova <naumarina@yandex.ru>
2023-09-12 16:02:43 +0800

    Fix calc.js: funcion calcGame. And add funcion calculator

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,23 +1,37 @@
-import { startGame, raund } from '../index.js';
-import { getRandomValue, calculator } from '../support.js';
+import { startGame, raundsCount } from '../index.js';
+import { getRandomValue } from '../support.js';
 
 const task = 'What is the result of the expression?';
-const expressions = [];
-const results = [];
-let expressionsAndResults = [];
+
+const calculator = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return null;
+  }
+};
 
 const calcGame = () => {
-  for (let i = 0; i < raund; i += 1) {
+  // const expressions = [];
+  // const results = [];
+  const expressionsAndResults = [];
+  for (let i = 0; i < raundsCount; i += 1) {
     const number1 = getRandomValue(1, 100);
     const number2 = getRandomValue(1, 100);
     const operators = ['+', '-', '*'];
     const operator = operators[getRandomValue(0, operators.length)];
     const expression = `${number1} ${operator} ${number2}`;
-    expressions.push(expression);
+    // expressions.push(expression);
     const result = String(calculator(number1, number2, operator));
-    results.push(result);
+    // results.push(result);
+    const exprRes = [expression, result];
+    expressionsAndResults.push(exprRes);
   }
-  expressionsAndResults = [expressions, results];
   startGame(expressionsAndResults, task);
 };
 
