ba5b0c0b8d04d9165f2e60dc241dec5c1c4d7303
Marina Garipova <naumarina@yandex.ru>
2023-09-12 16:05:49 +0800

    Fix gcd.js: funcion gcdGame. And add funcion gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,21 +1,36 @@
-import { startGame, raund } from '../index.js';
-import { getRandomValue, gcd } from '../support.js';
+import { startGame, raundsCount } from '../index.js';
+import { getRandomValue } from '../support.js';
 
 const task = 'Find the greatest common divisor of given numbers.';
-const expressions = [];
-const results = [];
-let expressionsAndResults = [];
+
+const gcd = (numberX, numberY) => {
+  let a = numberX;
+  let b = numberY;
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a %= b;
+    } else if (b > a) {
+      b %= a;
+    }
+  }
+  return a + b;
+};
 
 const gcdGame = () => {
-  for (let i = 0; i < raund; i += 1) {
+  // const expressions = [];
+  // const results = [];
+  const expressionsAndResults = [];
+  for (let i = 0; i < raundsCount; i += 1) {
     const number1 = getRandomValue(1, 100);
     const number2 = getRandomValue(1, 100);
     const expression = `${number1} ${number2}`;
-    expressions.push(expression);
+    // expressions.push(expression);
     const result = String(gcd(number1, number2));
-    results.push(result);
+    // results.push(result);
+    const exprRes = [expression, result];
+    expressionsAndResults.push(exprRes);
   }
-  expressionsAndResults = [expressions, results];
+  // expressionsAndResults = [expressions, results];
   startGame(expressionsAndResults, task);
 };
 
