61e31e2c8cc8ed45602806b2368207157a7aa056
Marina Garipova <naumarina@yandex.ru>
2023-09-12 16:07:44 +0800

    Fix prime.js: funcion primeGame. And add funcion isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,28 @@
 import { startGame } from '../index.js';
-import { forPridicat, isPrime } from '../support.js';
+import { forPridicat } from '../support.js';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-let expressionsAndResults = [];
+
+const isPrime = (number) => {
+  if (number === 1) {
+    return 'no';
+  }
+  const max = number;
+  const arr = [];
+  for (let i = 2; i < max; i += 1) {
+    const result = number % i;
+    arr.push(result);
+  }
+  if (arr.includes(0)) {
+    return 'no';
+  }
+  return 'yes';
+};
 
 const primeGame = () => {
-  expressionsAndResults = forPridicat(isPrime);
-  startGame(expressionsAndResults, task);
+  // let expressionsAndResults = [];
+  // expressionsAndResults = forPridicat(isPrime);
+  startGame(forPridicat(isPrime), task);
 };
 
 export default primeGame;
