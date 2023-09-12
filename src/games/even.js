a4aa82584db281ef8983ff52fae9a326a8dc9abd
Marina Garipova <naumarina@yandex.ru>
2023-09-12 16:31:28 +0800

    Fix even.js: add funcion isEven

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,14 @@
 import { startGame } from '../index.js';
-import { forPridicat, isEven } from '../support.js';
+import { forPridicat } from '../support.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
 
 const evenGame = () => {
   startGame(forPridicat(isEven), task);
