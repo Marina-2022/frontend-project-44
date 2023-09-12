5a0e0a29ce6d83288e7d5fefedc10861d1ad3305
Marina Garipova <naumarina@yandex.ru>
2023-09-12 16:33:04 +0800

    Fix support.js: removed funcions isEven

--- src/support.js
+++ src/support.js
@@ -1,10 +1,3 @@
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
-
 const getRandomValue = (min, max) => {
   const callMin = Math.ceil(min);
   const callmax = Math.floor(max);
@@ -14,18 +7,14 @@ const getRandomValue = (min, max) => {
 
 const forPridicat = (evenOrPrime) => {
   const expressionsAndResults = [];
-  // const expressions = [];
-  // const results = [];
   for (let i = 0; i < 3; i += 1) {
     const number = getRandomValue(1, 100);
     const expression = `${number}`;
-    // expressions.push(expression);
     const result = String(evenOrPrime(number));
-    // results.push(result);
     const exprRes = [expression, result];
     expressionsAndResults.push(exprRes);
   }
   return expressionsAndResults;
 };
 
-export { getRandomValue, isEven, forPridicat };
+export { getRandomValue, forPridicat };
