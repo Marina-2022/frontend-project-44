2c1d78a40348f52e083359b2a644338a22bd86b8
Marina Garipova <naumarina@yandex.ru>
2023-09-12 23:28:04 +0800

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const raundsCount = 3;
 const startGame = (expressionsAndResults, task) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}!`);
   console.log(task);
   for (let i = 0; i < raundsCount; i += 1) {
     const [question, answer] = expressionsAndResults[i];
@@ -14,7 +14,7 @@ const startGame = (expressionsAndResults, task) => {
     if (answer === userAnswer) {
       console.log('Correct!');
     } else if (answer !== userAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
