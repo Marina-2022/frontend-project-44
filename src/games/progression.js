aa117330871fd5c7f680c1d1d5b70230c5bcebdc
Marina Garipova <naumarina@yandex.ru>
2023-09-14 16:17:38 +0800

    Fix progression.js: Renamed consts.

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,13 +1,13 @@
 import { startGame, raundsCount } from '../index.js';
-import { getRandomValue } from '../support.js';
+import getRandomNumber from '../utils.js';
 
 const task = 'What number is missing in the progression?';
 
 const startArithmeticProgression = () => {
   const progressions = [];
-  const firstNumber = getRandomValue(1, 101);
-  const step = getRandomValue(1, 6);
-  const item = getRandomValue(5, 9);
+  const firstNumber = getRandomNumber(1, 101);
+  const step = getRandomNumber(1, 6);
+  const item = getRandomNumber(5, 9);
   let sum1 = firstNumber + step;
   progressions.push(firstNumber, sum1);
   for (let i = 0; i < item; i += 1) {
@@ -17,19 +17,18 @@ const startArithmeticProgression = () => {
   return progressions;
 };
 
-const progressionGame = () => {
-  const expressionsAndResults = [];
+const startProgressionGame = () => {
+  const raundInfo = [];
   const missNumber = '..';
   for (let i = 0; i < raundsCount; i += 1) {
     const arithmeticProgression = startArithmeticProgression();
-    const randomMissIndex = getRandomValue(0, arithmeticProgression.length - 1);
-    const result = `${arithmeticProgression[randomMissIndex]}`;
+    const randomMissIndex = getRandomNumber(0, arithmeticProgression.length - 1);
+    const rightAnswer = `${arithmeticProgression[randomMissIndex]}`;
     arithmeticProgression[randomMissIndex] = missNumber;
-    const expression = `${arithmeticProgression.join(' ')}`;
-    const exprRes = [expression, result];
-    expressionsAndResults.push(exprRes);
+    const question = `${arithmeticProgression.join(' ')}`;
+    raundInfo.push([question, rightAnswer]);
   }
-  startGame(expressionsAndResults, task);
+  startGame(raundInfo, task);
 };
 
-export default progressionGame;
+export default startProgressionGame;
