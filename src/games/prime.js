c0c5e80f136199fa8151a114c09467dd176642ac
Marina Garipova <naumarina@yandex.ru>
2023-09-14 16:15:13 +0800

    Move code with basic logic. Renamed consts.

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,26 +1,33 @@
 import { startGame } from '../index.js';
-import { forPridicat } from '../support.js';
+import getRandomNumber from '../utils.js';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (number) => {
-  if (number === 1) {
-    return 'no';
+const isPrime = (randomNumber) => {
+  if (randomNumber === 1) {
+    return false;
   }
-  const max = number;
+  const maxIndex = randomNumber;
   const arr = [];
-  for (let i = 2; i < max; i += 1) {
-    const result = number % i;
+  for (let i = 2; i < maxIndex; i += 1) {
+    const result = randomNumber % i;
     arr.push(result);
   }
   if (arr.includes(0)) {
-    return 'no';
+    return false;
   }
-  return 'yes';
+  return true;
 };
 
-const primeGame = () => {
-  startGame(forPridicat(isPrime), task);
+const startPrimeGame = () => {
+  const raundInfo = [];
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomNumber(1, 100);
+    const question = `${number}`;
+    const rightAnswer = String(isPrime(number) ? 'yes' : 'no');
+    raundInfo.push([question, rightAnswer]);
+  }
+  startGame(raundInfo, task);
 };
 
-export default primeGame;
+export default startPrimeGame;
