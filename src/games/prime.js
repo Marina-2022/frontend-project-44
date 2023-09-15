3d8fd96c1691419ea295c315325c58677aac1703
Marina Garipova <naumarina@yandex.ru>
2023-09-15 22:36:47 +0800

    Fix prime.js: add raundsCoun. fix const isPrimet

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,4 @@
-import { startGame } from '../index.js';
+import { startGame, raundsCount } from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
@@ -7,21 +7,18 @@ const isPrime = (randomNumber) => {
   if (randomNumber === 1) {
     return false;
   }
-  const maxIndex = randomNumber;
-  const arr = [];
-  for (let i = 2; i < maxIndex; i += 1) {
+  for (let i = 2; i <= Math.sqrt(randomNumber); i += 1) {
     const result = randomNumber % i;
-    arr.push(result);
-  }
-  if (arr.includes(0)) {
-    return false;
+    if (result === 0) {
+      return false;
+    }
   }
   return true;
 };
 
 const startPrimeGame = () => {
   const raundInfo = [];
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < raundsCount; i += 1) {
     const number = getRandomNumber(1, 100);
     const question = `${number}`;
     const rightAnswer = String(isPrime(number) ? 'yes' : 'no');
